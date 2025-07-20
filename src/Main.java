61b857d256456abb1a8a942034b2968abc988d00
kevin-creater <kevinjoseph2028@cs.sjcetpalai.ac.in>
2025-07-20 08:17:03 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
-
-    // TODO: Create a method to display student details
+    
 public class Main {
       public static void main(String[] args) {
+            class Student{
+       String name;
+       int rollnumber;
+       char grade;
+       Student(){
+           name="kevin joseph";
+           rollnumber=100;
+           grade='A';
+       }
+       public void displayDetails() {
+           System.out.println("Name:"+name);
+            System.out.println("Rollnumber:"+rollnumber);
+            System.out.println("Grade:"+grade);
+       }
+   }
+    public class Main{
+    public static void main(String[] args){
+        Student student=new Student();
+        student.displayDetails();
+    }
+}
         // TODO: Create a Student object and call the method to display details
     }
 }
